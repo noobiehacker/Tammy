a5199632072f54e937867ae0276702426dfa8069
noobiehacker <davidkwokhochan@gmail.com>
2015-01-30 01:24:34 -0800

    Added Report API Get and Post

--- server.js
+++ server.js
@@ -8,7 +8,8 @@
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
     var database = require('./config/database');
- 
+    var Report     = require('./app/models/report');
+
     // configuration =================
 
     mongoose.connect(database.url);     // connect to mongoDB
@@ -19,6 +20,46 @@
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
+    
+    //==========routes
+    var router = express.Router();              // get an instance of the express Router
+    app.use('/api', router);
+
+    // middleware to use for all requests
+    router.use(function(req, res, next) {
+        console.log('Something is happening.');
+        next(); // make sure we go to the next routes and don't stop here
+    });
+    
+    router.get('/', function(req, res) {
+        res.json({ message: 'hooray! welcome to our api!' });   
+    });
+
+    router.route('/reports')
+
+    .post(function(req, res) {
+        
+        var report = new Report();      // create a new instance of the Bear model
+        report.name = req.body.name  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        report.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'report created!' });
+        });
+        
+    })
+
+    .get(function(req, res) {
+        Report.find(function(err, reports) {
+            if (err)
+                res.send(err);
+
+            res.json(reports);
+        });
+    });
 
     // load the routes
     require('./app/routes')(app);
